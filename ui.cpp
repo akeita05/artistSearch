e61513c4ed84003a06e9fcfa3c06603e313e38ed
Kai Vedder <kawasaki.vedder@gmail.com>
2024-12-03 18:09:27 -0500

    added temporary ui files for building

---
+++ ui.cpp
@@ -0,0 +1,25 @@
+
+
+#include "ui.h"
+
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+void startUI() {
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Artist Search");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
---
+++ ui.h
@@ -0,0 +1,10 @@
+//
+// Created by itska on 12/3/2024.
+//
+
+#ifndef PROJECT3_UI_H
+#define PROJECT3_UI_H
+
+void startUI();
+
+#endif //PROJECT3_UI_H
